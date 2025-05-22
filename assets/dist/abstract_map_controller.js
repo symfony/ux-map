cea39b8c6f6c54d0512a24117bc608bb375f6970
Mart Kop <mart@insiders.nl>
2025-05-22 12:22:04 +0200

    [Map] add definition to after create events in controller

--- assets/dist/abstract_map_controller.js
+++ assets/dist/abstract_map_controller.js
@@ -43,7 +43,7 @@ class default_1 extends Controller {
     createInfoWindow({ definition, element, }) {
         this.dispatchEvent('info-window:before-create', { definition, element });
         const infoWindow = this.doCreateInfoWindow({ definition, element });
-        this.dispatchEvent('info-window:after-create', { infoWindow, element });
+        this.dispatchEvent('info-window:after-create', { infoWindow, definition, element });
         this.infoWindows.push(infoWindow);
         return infoWindow;
     }
@@ -74,7 +74,7 @@ class default_1 extends Controller {
         return ({ definition }) => {
             this.dispatchEvent(eventBefore, { definition });
             const drawing = factory({ definition });
-            this.dispatchEvent(eventAfter, { [type]: drawing });
+            this.dispatchEvent(eventAfter, { [type]: drawing, definition });
             draws.set(definition['@id'], drawing);
             return drawing;
         };

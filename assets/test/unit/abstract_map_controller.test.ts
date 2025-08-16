66896641f6b2e3f561c20bff2950d5a6605228e1
Hugo Alliaume <hugo@alliau.me>
2025-08-16 19:17:43 +0200

    Configure Vitest for unit and browser tests (use @puppeteer/browsers and webdriverio)

--- assets/test/unit/abstract_map_controller.test.ts
+++ assets/test/unit/abstract_map_controller.test.ts
@@ -1,8 +1,8 @@
 import { Application } from '@hotwired/stimulus';
 import { getByTestId, waitFor } from '@testing-library/dom';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-import { clearDOM, mountDOM } from '../../../../test/stimulus-helpers';
-import AbstractMapController from '../src/abstract_map_controller.ts';
+import { clearDOM, mountDOM } from '../../../../../test/stimulus-helpers';
+import AbstractMapController from '../../src/abstract_map_controller.ts';
 
 class MyMapController extends AbstractMapController {
     protected dispatchEvent(name: string, payload: Record<string, unknown> = {}): void {
